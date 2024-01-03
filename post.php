00b4883a5ab3ee0cbb9b5ba34938717556d51f0e
PEXXVIII <goblinslayer1848@gmail.com>
2024-01-03 15:48:24 +0700

    work4 post.php
    work4 post.php

---
+++ post.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<header>
+    <H1>Webboard PEXXVIII</H1>
+</header>
+<body  style="text-align: center;">
+    <?php
+    echo "ต้องการดูกระทู้ $_GET[id]";
+
+    ?>
+    <form>
+    <table style="border: 2px solid #D4D2F2; width: auto;" align="center">
+            <tr><td colspan="2" style="background-color: #6CD2FE;">แสดงความคิดเห็น</td></tr>
+
+            <tr><td></td><td><textarea type="text" name="text" size="30" required></textarea></td></tr>    
+            <tr><td colspan="2" align="center"><input type="submit" value="ส่งข้อความ"></td></tr>
+        </table>
+    </form>
+    
+</body>
+</html>
---
+++ verify.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Verify</title>
+</head>
+<body>
+<h1 style="text-align: center;">Webboard PEXXVIII</h1>
+    <hr>
+    <div style="text-align: center;">
+    เข้าสู่ระบบด้วย<br>
+    Login = <?php  echo $_POST['login']; ?> <br> 
+    password = <?php  echo $_POST['pwd']; ?> 
+    </div><br>
+   
+    
+</body>
+</html>
