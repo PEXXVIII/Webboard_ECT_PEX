527b070be01425a226cdb61f964ed20764d78923
PEXXVIII <goblinslayer1848@gmail.com>
2024-01-03 13:45:18 +0700

    Example 1 in php
    Example 1 in php

---
+++ a.php
@@ -0,0 +1,10 @@
+<?php
+$a = 4;
+$b = 10;
+
+echo "Resuit 1 = " . ($a + $b) . "<br>";
+echo "Resuit 1 = " . ($b - $a) . "<br>";
+echo "Resuit 1 = " . ($a * $b) . "<br>";
+echo "Resuit 1 = " . ($b / $a) . "<br>";
+
+?>
