c7841172edc3cba81a4b88761a1129b91763e21a
PEXXVIII <goblinslayer1848@gmail.com>
2024-01-17 16:36:37 +0700

    Work 07
    Work 07

--- index.php
+++ index.php
@@ -1,9 +1,12 @@
+<?php
+session_start();
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Webboard PEXXVIII</title>
+    <title>Webboard PEXXJAV</title>
 </head>
 <body>
     <h1 style="text-align: center;">Webboard PEXJAV </h1>
@@ -15,12 +18,32 @@
             <option value="general">เรื่องทั่วไป</option>
             <option value="study">เรื่องเรียน</option>
         </select>
-        <a href="login.html" style="float: right;">เข้าสู่ระบบ</a>
+
+        <?php
+        if(!isset($_SESSION['id'])){
+            echo "<a href=login.php style='float: right;'>เข้าสู่ระบบ</a>";
+        }else {
+            echo "<div  style ='float:right'>
+            ผู้ใช้งานระบบ : $_SESSION[username]&nbsp;&nbsp;
+            <a href=logout.php>ออกจากระบบ</a>
+            </div><br>
+            <a href=newpost.php>สร้างกระทู้ใหม่</a>";
+
+        }
+        ?>
+
     </form>
     <ul>
+
+
         <?php
         for($i= 1; $i <= 10; $i++){
-            echo "<li><a href=post.php?id=$i>กระทู้ที่ $i</a></li>";
+            echo "<li><a href=post.php?id=$i>กระทู้ที่ $i</a>";
+            if(isset($_SESSION['id']) && $_SESSION['role']=='a'){
+                echo "&nbsp;&nbsp;<a href=delete.php?id=$i>ลบ</a>";
+
+            }
+            echo "</li>";
 
 
         }
@@ -30,6 +53,7 @@
         <li><a href="post.php?id=3">กระทู้ที่ 3</a></li>
         <li><a href="post.php?id=4">กระทู้ที่ 4</a></li>
         <li><a href="post.php?id=5">กระทู้ที่ 5</a></li> -->
+
     </ul>
 
 </body>
--- verify.php
+++ verify.php
@@ -1,3 +1,10 @@
+<?php
+session_start();
+if(isset($_SESSION['id'])){
+    header("location:index.php");
+    die();
+}
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -15,8 +22,14 @@
     $Login= ($_POST['login']);
     $password = ($_POST['pwd']);
     if($Login == "admin" && $password == "ad1234"){
+        $_SESSION["username"] ="admin";
+        $_SESSION["role"] = "a";
+        $_SESSION["id"] = session_id();
         echo "ยินดีตอนรับคุณ ADMIN";
     }elseif ($Login == "member" && $password == "mem123"){
+        $_SESSION["username"] = "member";
+        $_SESSION["role"] = "m";
+        $_SESSION["id"] = session_id();
         echo "ยินดีต้อนรับคุณ MAMBER";
     }else 
     echo "ชื่อบัญชีหรือรหัสผ่านไม่ถูกต้อง";
