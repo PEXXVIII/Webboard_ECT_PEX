5ae63ed68f66dcc309dbb55b7d569dff2e16009d
PEXXVIII <goblinslayer1848@gmail.com>
2024-02-28 21:37:11 +0700

    work 10 and 11
    work 10 and 11

--- index.php
+++ index.php
@@ -3,63 +3,70 @@ session_start();
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="home.css">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
-    
-    <title>Webboard PEXXJAV</title>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL"
+        crossorigin="anonymous"></script>
+
+        <title>Webboard PEXJAV</title>
 </head>
 <body>
-     <div class="container-lg">
-    <h1 style="text-align: center;" class"mt-3">Webboard PEXJAV </h1>
-
-    <?php @include "nav.php" ?>
+    <div class="container-lg">
+    <h1 style="text-align: center;" class="mt-3">Webboard PEXJAV</h1>
+<?php include "nav.php" ?>
 
 <div class="mt-3 d-flex justify-content-between">
+<div>
   <div>
   <label>หมวดหมู่</label>
-  <span class="dropdown">
-  <button class="btn btn-light btn-sm dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+   <span class="dropdown">
+   <button class="btn btn-light dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
     --ทั้งหมด--
-  </button>
-  <ul class="dropdown-menu" aria-labelledby="Button2">
-    <li><a href="#" class="dropdown-item">ทั้งหมด</a></li>
+   </button>
+   <ul class="dropdown-menu" aria-labelledby="Button2">
+    <li><a class="dropdown-item" href="#">ทั้งหมด</a></li>
     <?php
-      $conn=new PDO("mysql:host=localhost;dbname=webboard;charset=utf8","root","");
+      $conn=new PDO("mysql:host=localhost;dbname=webboard;
+      charset=utf8","root","");
       $sql="SELECT * FROM category";
-    foreach($conn->query($sql) as $row){
-      echo"<li><a class='dropdown-item' href=#>$row[name]</a></li>";
-    }
-    $conn=null;
+      foreach($conn->query($sql) as $row){
+        echo"<li>< class=dropdown-item href=#>$row[name]></a></li>";
+      }
+      $conn=null;
     ?>
-    </ul>
-    
+  </ul>
 </span>
 </div>
-<?php if(isset($_SESSION['id'])){ ?>
+</div>
+<?php if(isset($_SESSION['id'])){?>
 <div>
-<a href="newpost.php" class="btn btn-success btn-sm">
-<i class="bi bi-plus"></i> สร้างกระทู้ใหม่</a>
-</div>
-<?php } ?>
+    <a href="newpost.php" class="btn btn-success btn-sm"><i class="bi bi-plus-lg"></i> สร้างกระทู้ใหม่</a>
+  </div>
+  <?php } ?>
 </div>
 
-   <table class="table table-striped mt-4">
-   <?php 
-$conn = new PDO("mysql:host=localhost;dbname=webboard;charset=utf8", "root", "");
-$sql = "SELECT t3.name, t1.title, t1.id, t2.login, t1.post_date FROM post as t1
-        INNER JOIN user as t2 ON (t1.user_id = t2.id)
-        INNER JOIN category as t3 ON (t1.cat_id = t3.id) ORDER BY t1.post_date DESC";
-$result = $conn->query($sql);
-while ($row = $result->fetch()) {
-    echo "<tr><td>[$row[0]] <a href=post.php?id=$row[2] style=text-decoration:none>$row[1]</a><br>$row[3]-$row[4]</td></tr>";
-}
-   $conn=null;
-      ?>
-   </table>
-
+<table class="table table-striped  ">
+        <?php
+            $conn=new PDO("mysql:host=localhost;dbname=webboard;charset=utf8","root","");
+            $sql="SELECT t3.name,t1.title,t1.id,t2.login,t1.post_date FROM post as t1
+            INNER JOIN user as t2 ON (t1.user_id = t2.id)
+            INNER JOIN category as t3 ON (t1.cat_id = t3.id) ORDER BY t1.post_date DESC";
+            $result=$conn->query($sql);
+            while($row = $result->fetch()){
+                echo "<tr><td>[ $row[0] ] <a href=post.php?id=$row[2]
+                style=text-decoration:none>$row[1]</a><br>$row[3] - $row[4]</td></tr>";
+            }
+            $conn=null;
+        ?>
+</table>
+    </div>
 </body>
 </html>
--- register_save.php
+++ register_save.php
@@ -1,26 +1,27 @@
 <?php
-session_start();
-$login=$_POST['login'];
-$password=$_POST['pwd'];
-$name=$_POST['name'];
-$gender=$_POST['gender'];
-$email=$_POST['email'];
+    session_start();
+    $login=$_POST['login'];
+    $password=sha1($_POST['pwd']);
+    $name=$_POST['name'];
+    $gender=$_POST['gender'];
+    $email=$_POST['email'];
 
-$conn=new PDO("mysql:host=localhost;dbname=webboard;charset=utf8","root","");
-$sql="SELECT *FROM user where  login='$login'";
-$result=$conn->query($sql);
-if($result->rowCount()==1){
-    $_SESSION['add_login']="error";
-}else{
-    $sql="INSERT INTO user (logim,password,name,gender,email,role)
-    VALUES ('$login','$password','$name','$gender','$email','m',)";
-    $conn->exec($sql1);
-    $_SESSION['add_login']="success";
+    $conn=new PDO("mysql:host=localhost;dbname=webboard;charset=utf8","root","");
 
-}
-$conn=null;
-header("Location: login.php");
-die();
+    $sql="SELECT * FROM user where login='$login'";
 
+    $result=$conn->query($sql);
+    if($result->rowCount()==1){
+        $_SESSION['add_login']="error";
+    }else{
+        $sql1="INSERT INTO user (login, password, name, gender, email, role)
+        VALUES('$login','$password','$name','$gender','$email','m')";
+        $conn->exec($sql1);
+        $_SESSION['add_login']="success";
+    }
+    $conn=null;
+
+    header("Location: register.php");  
+    die();
 
 ?>
